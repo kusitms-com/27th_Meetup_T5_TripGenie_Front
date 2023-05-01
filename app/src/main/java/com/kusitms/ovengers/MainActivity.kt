37dba36e46b6c76ec749ec98ad50ec9b7fe0b87c
Jeong <yuu-jeong@naver.com>
2023-05-02 02:46:30 +0900

    Feat : MainActivity, SplashActivity UI

--- app/src/main/java/com/kusitms/ovengers/MainActivity.kt
+++ app/src/main/java/com/kusitms/ovengers/MainActivity.kt
@@ -2,10 +2,13 @@ package com.kusitms.ovengers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnLogin = findViewById<ImageButton>(R.id.btn_login)
     }
 }
